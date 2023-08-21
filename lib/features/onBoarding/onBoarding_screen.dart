2460cdcf8fae08064c1d5cbf807322912bc4c14f
Bishal <bishalbera2@gmail.com>
2023-08-21 08:24:11 +0530

    onBoarding screen added

--- lib/features/onBoarding/onBoarding_screen.dart
+++ lib/features/onBoarding/onBoarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -10,6 +11,66 @@ class OnBoardingScreen extends StatefulWidget {
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/onboarding.jpg',
+              height: MediaQuery.of(context).size.height * 0.6,
+            ),
+            // SizedBox(
+            //   height: 20,
+            // ),
+            Text(
+              'Welcome to \n'
+              'Book Verse',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'A place where you can connect with your friends and family',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepPurple,
+                elevation: 13,
+                shadowColor: Colors.deepPurpleAccent.withOpacity(0.9),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 16,
+                ),
+              ),
+              child: Text(
+                'Get Started',
+                style: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
